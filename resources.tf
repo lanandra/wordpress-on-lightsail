698aebc418a3c6ab9f7c10bd6653b95722cd4bbc
lanandra <luthfi.anandra@gmail.com>
2022-11-26 23:39:39 +0700

    Fix typo and add dependacy

--- resources.tf
+++ resources.tf
@@ -1,23 +1,28 @@
 # Create lighstail instance with wordpress blueprint
-resource "aws_lightsail_instance" "wp_lighstail_demo" {
-  name              = "wp-lighstail-demo"
+resource "aws_lightsail_instance" "wp_lightsail_demo" {
+  name              = "wp-lightsail-demo"
   availability_zone = var.availability_zone[0]
   blueprint_id      = var.wp_blueprint_id
   bundle_id         = var.bundle_id
   key_pair_name     = var.lightsail_key_pair
 
   tags = {
-    Name                   = "wp-lighstail-demo"
+    Name                   = "wp-lightsail-demo"
     "lanandra:environment" = "Sandbox"
     "lanandra:owner"       = "Luthfi"
   }
 }
 
-resource "aws_lightsail_static_ip" "wp_lighstail_demo" {
-  name = "wp-lighstail-demo"
+resource "aws_lightsail_static_ip" "wp_lightsail_demo" {
+  name = "wp-lightsail-demo"
 }
 
-resource "aws_lightsail_static_ip_attachment" "wp_lighstail_demo" {
-  static_ip_name = aws_lightsail_static_ip.wp_lighstail_demo.id
-  instance_name  = "wp-lighstail-demo"
+resource "aws_lightsail_static_ip_attachment" "wp-lightsail-demo" {
+  static_ip_name = aws_lightsail_static_ip.wp_lightsail_demo.id
+  instance_name  = "wp-lightsail-demo"
+
+  depends_on = [
+    aws_lightsail_instance.wp_lightsail_demo,
+    aws_lightsail_static_ip.wp_lightsail_demo
+  ]
 }
